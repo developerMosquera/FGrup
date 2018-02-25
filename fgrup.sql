0fbb9d659d4190e14e11b516017396c9d608aff8
developerMosquera <julian.barrios.234@gmail.com>
2018-02-25 10:21:50 -0500

    [add] sql

--- fgrup.sql
+++ fgrup.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 23-02-2018 a las 22:53:11
--- Versión del servidor: 10.0.33-MariaDB-0ubuntu0.16.04.1
--- Versión de PHP: 7.0.22-0ubuntu0.16.04.1
+-- Tiempo de generación: 25-02-2018 a las 15:20:44
+-- Versión del servidor: 5.7.20
+-- Versión de PHP: 7.1.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -43,11 +45,7 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`ID`, `CLIENTE`, `NIT`, `DIRECCION`, `TELEFONO`, `CORREO_1`, `CORREO_2`, `FECHA_CREACION`, `ESTADO`) VALUES
-(1, 'Farmacias emoty', '12546988-1', 'cr 54 # 32 - 40', 7584566, 'femoty@hotmail.com', '', '0000-00-00 00:00:00', 1),
-(2, 'Farmatodo', '15152154-1', 'Calle 26 # 32 - 21', 7588565, 'farmatodo@gmail.com', '', '0000-00-00 00:00:00', 1),
-(3, 'Quimicos diarios', '52152477-4', 'cr 32 # 26 - 32', 6325212, 'qumic@gmail.com', '', '0000-00-00 00:00:00', 1),
-(4, 'asdsad', 'sdf', 'sdf', 345345, 'dfd@g.com', '', '2018-02-22 23:21:48', 1),
-(6, 'prueba concliente', 'as34', 'asdasda', 23423423, 'sadad@g.ocm', '', '2018-02-22 23:31:08', 1);
+(1, 'Ropsohn', '342424-1', 'calle false 123', 24342433, 'ropsohn@hotmail.com', '', '2018-02-24 16:56:07', 1);
 
 -- --------------------------------------------------------
 
@@ -71,7 +69,9 @@ CREATE TABLE `equipos` (
 --
 
 INSERT INTO `equipos` (`ID`, `ID_CLIENTE`, `EQUIPO`, `SERIAL_SISTEMA`, `SERIAL_REAL`, `MODELO`, `FECHA_CREACION`, `ESTADO`) VALUES
-(21, 2, 'pruebaFinal', '56456351851833131', '564563erer33131', 'sdf33s', '2018-02-23 22:18:35', 1);
+(1, 1, 'Disolutor', '2344346185', '23443dfre', '45677', '2018-02-24 16:56:07', 1),
+(2, 1, 'sdfadf', '1914614196234', 'sadfadsf234', 'fasdf', '2018-02-24 17:04:35', 1),
+(3, 1, 'fgfgh', '4196745645', 'dsfg45645', 'dgdfg', '2018-02-24 17:06:08', 1);
 
 -- --------------------------------------------------------
 
@@ -123,16 +123,9 @@ CREATE TABLE `programador` (
 --
 
 INSERT INTO `programador` (`ID`, `LLAVE`, `SERIAL_SISTEMA`, `ID_SERVICIO`, `ID_CLIENTE`, `ID_CREADOR`, `ID_TECNICO`, `ESTADO`, `FECHA_PROGRAMADOR`, `COMBINACION`, `ESTADO_COMBINACION`, `FECHA_CREACION`) VALUES
-(1, '1254512511251', '254512511251', 1, 1, 1, 1, 1, '2018-02-15', 0, 0, '2018-02-27 00:00:00'),
-(2, '4561820722876106575463553551', '456182072287610657546355355', 1, 1, 1, NULL, 1, '2018-02-25', NULL, NULL, '2018-02-22 23:08:40'),
-(3, '1943342348', '194334234', 8, 3, 1, NULL, 1, '2018-02-25', NULL, NULL, '2018-02-22 23:17:48'),
-(4, '119410', '1194', 10, 3, 1, NULL, 1, '2018-02-25', NULL, NULL, '2018-02-22 23:18:34'),
-(5, '194634519469', '19463451946', 9, 2, 1, NULL, 1, '2018-02-25', NULL, NULL, '2018-02-22 23:19:25'),
-(6, '191461194610', '1914611946', 10, 3, 1, NULL, 1, '2018-02-25', NULL, NULL, '2018-02-22 23:21:12'),
-(7, '19146119463410', '191461194634', 10, 4, 1, NULL, 1, '2018-02-25', NULL, NULL, '2018-02-22 23:21:48'),
-(8, '34223423518181', '3422342351818', 1, 4, 1, NULL, 1, '2018-02-25', NULL, NULL, '2018-02-22 23:25:00'),
-(9, '161821521315143129514205231', '16182152131514312951420523', 1, 6, 1, NULL, 1, '2018-02-25', NULL, NULL, '2018-02-22 23:31:08'),
-(10, '564563518518331311', '56456351851833131', 1, 2, 1, NULL, 1, '2018-02-26', NULL, NULL, '2018-02-23 22:18:35');
+(1, '23443461851', '2344346185', 1, 1, 1, NULL, 1, '2018-02-27', NULL, NULL, '2018-02-24 16:56:07'),
+(2, '19146141962341', '1914614196234', 1, 1, 1, NULL, 1, '2018-02-27', NULL, NULL, '2018-02-24 17:04:35'),
+(3, '41967456451', '4196745645', 1, 1, 1, NULL, 1, '2018-02-27', NULL, NULL, '2018-02-24 17:06:08');
 
 -- --------------------------------------------------------
 
@@ -155,13 +148,6 @@ CREATE TABLE `programador_historial` (
   `FECHA_CREACION` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `programador_historial`
---
-
-INSERT INTO `programador_historial` (`ID`, `LLAVE`, `SERIAL`, `ID_SERVICIO`, `ID_CLIENTE`, `ID_CREADOR`, `ID_TECNICO`, `ESTADO`, `FECHA`, `COMBINACION`, `ESTADO_COMBINACION`, `FECHA_CREACION`) VALUES
-(1, '1254512511251', '254512511251', 1, 1, 1, 1, 1, '2018-02-28', 0, 0, '2018-02-20 00:00:00');
-
 -- --------------------------------------------------------
 
 --
@@ -183,21 +169,21 @@ CREATE TABLE `servicios` (
 
 INSERT INTO `servicios` (`ID`, `SERVICIO`, `ALIAS`, `PERIOCIDAD`, `CONBINACION`, `ESTADO`) VALUES
 (1, 'Mantenimiento', 'Mantenimiento', 6, '0', 1),
-(2, 'Calibraciòn', 'Calibraciòn', 6, '0', 1),
+(2, 'Calibración', 'Calibración', 6, '0', 1),
 (3, 'Correctivo', 'Correctivo', 0, '0', 1),
-(4, 'Instalaciòn', 'Instalaciòn', 0, '0', 1),
-(5, 'Calificaciòn', 'Calificaciòn', 12, '0', 1),
-(6, 'Garantìa', 'Garantìa', 0, '0', 1),
-(7, 'Retrabajo', 'Garantìa', 0, '0', 1),
-(8, 'Revisiòn', 'Revisiòn', 0, '0', 1),
-(9, 'Regeneraciòn de resinas', 'Regeneraciòn de resinas', 12, '0', 1),
-(10, 'Capacitaciòn', 'Capacitaciòn', 0, '0', 1),
+(4, 'Instalación', 'Instalación', 0, '0', 1),
+(5, 'Calificación', 'Calificación', 12, '0', 1),
+(6, 'Garantìa', 'Garantía', 0, '0', 1),
+(7, 'Retrabajo', 'Garantía', 0, '0', 1),
+(8, 'Revisión', 'Revisión', 0, '0', 1),
+(9, 'Regeneración de resinas', 'Regeneración de resinas', 12, '0', 1),
+(10, 'Capacitación', 'Capacitación', 0, '0', 1),
 (11, 'Diagnostico', 'Diagnostico', 0, '0', 1),
-(12, 'Validaciòn', 'Validaciòn', 24, '0', 1),
-(13, 'Mantenimiento y calibraciòn', 'Mantenimiento y calibraciòn', 6, '1,2', 1),
-(14, 'Mantenimiento, Calibraciòn y Calificaciòn', 'Mantenimiento, Calibraciòn y Calificaciòn', 12, '1,2,5', 1),
-(15, 'Mantenimiento, Calibraciòn y Validaciòn', 'Mantenimiento, Calibraciòn y Validaciòn', 24, '1,2,12', 1),
-(16, 'Mantenimiento, Calibraciòn y Regeneraciòn de resinas', 'Mantenimiento, Calibraciòn y Regeneraciòn de resinas', 12, '1,2,9', 1);
+(12, 'Validación', 'Validación', 24, '0', 1),
+(13, 'Mantenimiento y calibración', 'Mantenimiento y calibración', 6, '1,2', 1),
+(14, 'Mantenimiento, Calibración y Calificación', 'Mantenimiento, Calibración y Calificación', 12, '1,2,5', 1),
+(15, 'Mantenimiento, Calibración y Validación', 'Mantenimiento, Calibración y Validación', 24, '1,2,12', 1),
+(16, 'Mantenimiento, Calibración y Regeneración de resinas', 'Mantenimiento, Calibración y Regeneración de resinas', 12, '1,2,9', 1);
 
 -- --------------------------------------------------------
 
@@ -222,7 +208,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`ID`, `USUARIO`, `CONTRASENA`, `NOMBRE`, `PERFIL`, `PERMISOS`, `ESTADO`, `FECHA_VENCIMIENTO`, `TOKEN`) VALUES
-(1, 'julian', '$2y$10$PeBxGU/nPcL7uZHI6kCtze.8ltkJoCbGgKWniaL/1Ir4u4ghXcDKi', 'Andres Julian Mosquera', 1, 'config=0,usuarios=0,', 1, '2019-10-31', '1bd0df3c5442a11f96474cbbcabb1b7f'),
+(1, 'julian', '$2y$10$PeBxGU/nPcL7uZHI6kCtze.8ltkJoCbGgKWniaL/1Ir4u4ghXcDKi', 'Andres Julian Mosquera', 1, 'config=0,usuarios=0,', 1, '2019-10-31', '1e3cedb5de2fe8acdff2795aeb6dd851'),
 (2, 'ingrid', '34fsdfsf', 'Ingrid Gonzalez', 1, '', 1, '2018-08-31', 'dsfsdfgg');
 
 --
@@ -294,37 +280,45 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT de la tabla `equipos`
 --
 ALTER TABLE `equipos`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT de la tabla `estados`
 --
 ALTER TABLE `estados`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT de la tabla `programador`
 --
 ALTER TABLE `programador`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT de la tabla `programador_historial`
 --
 ALTER TABLE `programador_historial`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `servicios`
 --
 ALTER TABLE `servicios`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
